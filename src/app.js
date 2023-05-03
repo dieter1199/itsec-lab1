bb9a9a06a15904eccf56adc4388d68487a8e74f2
Dieter Holstein <dihoit00@hs-esslingen.de>
2023-05-03 13:30:26 +0000

    /secret

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-
 var db = require('./helpers/db');
 
 // Add new files containing routes here
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 const homeRouter = require('./routes/home');
+const secretRouter = require('./routes/secret'); 
 
 var app = express();
 
@@ -38,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/', authRouter);
 app.use('/', homeRouter);
+app.use('/', secretRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,7 +55,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 db.connect();
 
